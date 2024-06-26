a3f62daccb64d295884f72b642e4301ea434335e
Sai Bhargav Reddy Maddireddy <51699459+bhargav26reddy@users.noreply.github.com>
2024-06-25 22:39:05 -0500

    Time: 57 ms (19.75%), Space: 48.7 MB (55.16%) - LeetHub

---
+++ 2619-array-prototype-last/2619-array-prototype-last.js
@@ -0,0 +1,16 @@
+/**
+ * @return {null|boolean|number|string|Array|Object}
+ */
+Array.prototype.last = function() {
+    let arr = this
+     if(arr.length){
+        return arr[arr.length-1]
+     }else{
+        return -1
+     }
+};
+
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
