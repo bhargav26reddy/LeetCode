9aa13fe3f3b08861de2b4bc1198d767258b9cb65
Sai Bhargav Reddy Maddireddy <51699459+bhargav26reddy@users.noreply.github.com>
2024-08-14 22:09:35 -0500

    Time: 59 ms (56.89%), Space: 51 MB (46.14%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.js
@@ -0,0 +1,21 @@
+var simplifyPath = function(path) {
+    const stack = [];
+    const portion = path.split("/");
+    for (let p of portion){
+        if (p === ".."){
+            if (stack.length > 0){
+                stack.pop();
+            }
+        }
+        else if (p === "." || p == ""){
+            continue;
+        }
+        else{
+            stack.push(p);
+        }
+    }
+    if (stack.length == 0) return "/";
+    else{
+        return "/" + stack.join("/");
+    }
+};
